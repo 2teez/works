78d3cb3367b5fbc12c488142af63bfd0b8ab8334
Lina Zhao <lina.lastname@gmail.com>
2012-07-29 23:28:06 +0800

    test

---
+++ process.pl
@@ -0,0 +1,33 @@
+#!/usr/bin/env perl
+
+use strict;
+use warnings;
+use autodie qw(open close);
+use Carp qw(croak);
+use 5.012;
+
+my $template_file="template";
+my $h_data_file="h_data";
+
+open my $fh2, '<', $h_data_file;
+
+
+while(<$fh2>){
+	my ($h11,$h21,$h22,$h33)=split ' ', $_;
+	&substitute($h11,$h21,$h22,$h33);
+	#say $h11,$h21,$h22,$h33;
+}
+
+sub substitute{
+	my($h11,$h21,$h22,$h33)=@_;
+	open my $fh1, '<', $template_file;
+
+	while(<$fh1>){
+		s/\$h11/$h11/g;	
+		s/\$h21/$h21/g;	
+		s/\$h22/$h22/g;
+		s/\$h33/$h33/g;
+		print $_;
+	};
+		
+}
